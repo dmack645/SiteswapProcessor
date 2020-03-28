cc5b4047584aa53f8f109b8677b2d23a97f5191c
David <dcmakie@gmail.com>
2020-03-27 21:29:02 -0700

    Troubleshooting

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ else:
 try:
     subprocess.check_call("{} -m ensurepip --default-pip".format(callPython), shell = True)
     subprocess.check_call("{} -m pip install pyperclip".format(callPython), shell = True)
-    siteswapUI.main(True)
 except:
     print("Error installing pip or pyperclip")
     siteswapUI.main(False)
-
+else:
+    siteswapUI.main(True)
 
 
 
